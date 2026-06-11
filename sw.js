c6c636827a119f4d4f77b48944b6525ebabcefbc
Granitabazi <granit.abazi@fireromandie.ch>
2026-06-11 18:52:15 +0200

    Corriger la déconnexion automatique

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = "tempo-rh-v2";
+const CACHE = "tempo-rh-v3";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./cloud.js","./config.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET" || new URL(event.request.url).origin !== location.origin) return;
   event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response; }).catch(()=>caches.match(event.request)));
